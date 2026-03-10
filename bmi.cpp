5dd36f0d8fa6171426fce09940b67006aea3c7ee
Martzz-a <maritzafidelyah@gmail.com>
2026-03-10 07:53:04 +0700

    Menambahkan Fungsi Status BMI

--- bmi.cpp
+++ bmi.cpp
@@ -4,3 +4,14 @@ using namespace std;
 float rumus_BMI (float a, float b){
     return a/(b*b);
 }
+
+string status_BMI (float r){
+    if (r < 18.5)
+        return "Berat Badan Kurang";
+    else if (r < 25)
+        return "Berat Badan Normal";
+    else if (r < 30)
+        return "Berat Badan Kelebihan";
+    else
+        return "Berat Badan Obesitas";
+}
